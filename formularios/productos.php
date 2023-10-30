8c9aa2d35c6e569dc759ef51ea151c7011b692cd
migurm <migue.rueda.90@gmail.com>
2023-10-30 14:51:57 +0100

    bases

--- formularios/productos.php
+++ formularios/productos.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos</title>
-    <?php require 'php/funciones.php' ?>
 </head>
 <body>
-	
+	<form action="" method="POST">
+		<fieldset>
+			<legend>
+				Productos
+			</legend>
+			<label>
+				Nombre del producto:
+			</label>
+			<input type="text" name="nombre_producto">
+			<label>
+				Precio: 
+			</label>
+			<input type="text" name="precio_producto">
+			<label>
+				Descripción:
+			</label>
+			<input type="text" name="descripcion_producto">
+			<label>
+				Cantidad:
+			</label>
+			<input type="text" name="cantidad_producto">
+			<input type="submit" value="Registrar">
+		</fieldset>
+	</form>
     
 </body>
 </html>
---
+++ formularios/usuarios.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+<body>
+<form action="" method="POST">
+		<fieldset>
+			<legend>
+				Usuarios
+			</legend>
+			<label>
+				Nombre:
+			</label>
+			<input type="text" name="nombre_usuario">
+			<label>
+				Contreaseña: 
+			</label>
+			<input type="text" name="contrasena_usuario">
+			<label>
+				Fecha de nacimiento:
+			</label>
+			<input type="date" name="fecha_nacimiento">
+            <input type="submit" value="Registrar">
+		</fieldset>
+	</form>
+    
+</body>
+</html>
