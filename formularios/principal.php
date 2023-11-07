e8773907a1f5917e02e16095aa23640246c07e00
Miguel Angel Rueda Moreno <migue.rueda.90@gmail.com>
2023-11-07 20:34:29 +0100

    Pagina principal con tabla de productos de bbdd, SCRIPT 30 INSERTS

--- formularios/principal.php
+++ formularios/principal.php
@@ -20,14 +20,40 @@
         $usuario = $_SESSION["usuario"] = "invitado";
     }
     ?>
-
-
-
-    
     <div class="container">
         <h1>Página principal</h1>
         <h2>Bienvenid@ <?php echo $usuario ?></h2>
     </div>
+    <div>
+        <?php
+        $sql = "SELECT * FROM productos";
+        $resultado = $conexion -> query($sql);
+        ?>
+
+        <table class="table table-hover">
+            <thead class="table table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Artículo</th>
+                    <th>Precio</th>
+                    <th>Descripcion</th>
+                    <th>Cantidad</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+        while($fila = $resultado -> fetch_assoc()){/* fetch_assoc() crea una especie de array asociativo con las filas*/
+            echo "<tr>";
+            echo "<td>".$fila["idProducto"]."</td>";
+            echo "<td>".$fila["nombreProducto"]."</td>";
+            echo "<td>".$fila["precio"]."</td>";
+            echo "<td>".$fila["descripcion"]."</td>";
+            echo "<td>".$fila["cantidad"]."</td>";
+            //Aquí faltaría lo de las fotos, pero para esto habría que ampliar el formulario
+            echo "</tr>";
+        } 
+        ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
